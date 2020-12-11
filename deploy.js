bb17edf510fffbadfe3cdf32ac421d3a55bea1d6
James Nicolas <jamesnicolas@me.com>
2020-12-11 16:50:37 -0500

    test deploy

--- deploy.js
+++ deploy.js
@@ -1,23 +1,32 @@
 const simpleGit = require('simple-git');
 const child_process = require('child_process');
+const { exit } = require('process');
 
 // create SimpleGit instance with shousetsubook directory
 console.log(process.cwd() + '/dist')
-shousetsubook = simpleGit({baseDir: process.cwd() + '/dist'});
-git.status(onStatus).revparse('HEAD', onRevparse)
+var dist = simpleGit({baseDir: process.cwd() + '/dist'});
+var shousetsubook = simpleGit();
+shousetsubook.status(onStatus).revparse('HEAD', onRevparse);
 function onStatus (err, statusResult) {
     var gitConditions = [];
     if (!statusResult.isClean()) {
-        gitConditions.push('working directory is not clean')
+        gitConditions.push('working directory is not clean');
     }
     if (statusResult.current !== "main") {
-        gitConditions.push('current branch is not main')
+        gitConditions.push('current branch is not main');
+    }
+    if (statusResult.tracking !== "origin/main") {
+        gitConditions.push('current branch is not tracking origin/main');
+    }
+    if (statusResult.ahead !== 0 && statusResult.behind !== 0) {
+        gitConditions.push('current branch is not up-to-date with origin/main');
     }
     if (gitConditions.length > 0) {
-        console.log('Could not deploy because ' + gitConditions.join(' and '))
+        console.log('Could not deploy because ' + gitConditions.join(' and '));
+        exit(1);
     }
     child_process.execSync('npm run build',{stdio:[0,1,2]});
 }
 function onRevparse (err, commit) {
-    shousetsubook.add('./*').commit(commit).push('origin','gh-pages')
+    dist.add('./*').commit(commit).push('origin','gh-pages')
 }
