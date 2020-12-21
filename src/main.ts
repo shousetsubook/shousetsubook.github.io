b8fcd08a8de81e4943cac23aec1406e1999b2059
James Nicolas <jamesnicolas@me.com>
2020-12-21 16:50:42 -0500

    remove debug global vars

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
 import store from './store'
-import { kutenkara } from './helpers/aozora'
-
-(window as any).kutenkara = kutenkara;
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
